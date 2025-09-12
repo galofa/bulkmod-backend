53dd9fb200163045a50f639b325631a06caeaf82
galofa <nebulafa@gmail.com>
2025-09-12 01:41:43 -0300

    updated database structure + reset

--- prisma/migrations/20250821161829_init/migration.sql
+++ prisma/migrations/20250821161829_init/migration.sql
@@ -22,7 +22,7 @@ CREATE TABLE "public"."user_sessions" (
 );
 
 -- CreateTable
-CREATE TABLE "public"."playlists" (
+CREATE TABLE "public"."modlists" (
     "id" SERIAL NOT NULL,
     "name" VARCHAR(100) NOT NULL,
     "description" VARCHAR(500),
@@ -30,21 +30,19 @@ CREATE TABLE "public"."playlists" (
     "user_id" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "playlists_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "modlists_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "public"."playlist_mods" (
+CREATE TABLE "public"."modlist_mods" (
     "id" SERIAL NOT NULL,
-    "playlist_id" INTEGER NOT NULL,
+    "modlist_id" INTEGER NOT NULL,
     "mod_slug" VARCHAR(100) NOT NULL,
     "mod_title" VARCHAR(255) NOT NULL,
     "mod_icon_url" VARCHAR(500),
     "mod_author" VARCHAR(100) NOT NULL,
     "added_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "playlist_mods_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "modlist_mods_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -54,13 +52,11 @@ CREATE UNIQUE INDEX "users_username_key" ON "public"."users"("username");
 CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "playlist_mods_playlist_id_mod_slug_key" ON "public"."playlist_mods"("playlist_id", "mod_slug");
+CREATE UNIQUE INDEX "modlist_mods_modlist_id_mod_slug_key" ON "public"."modlist_mods"("modlist_id", "mod_slug");
 
 -- AddForeignKey
 ALTER TABLE "public"."user_sessions" ADD CONSTRAINT "user_sessions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "public"."playlists" ADD CONSTRAINT "playlists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "public"."playlist_mods" ADD CONSTRAINT "playlist_mods_playlist_id_fkey" FOREIGN KEY ("playlist_id") REFERENCES "public"."playlists"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."modlists" ADD CONSTRAINT "modlists_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."modlist_mods" ADD CONSTRAINT "modlist_mods_modlist_id_fkey" FOREIGN KEY ("modlist_id") REFERENCES "public"."modlists"("id") ON DELETE CASCADE ON UPDATE CASCADE;
